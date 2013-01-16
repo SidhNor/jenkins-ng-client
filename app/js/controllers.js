8f7c65ce6d0bdf95cbfa28cdc2dc40682f880ea6
Gleb Godonoga <gleb.godonoga@gmail.com>
2013-01-16 18:34:15 +0200

    Small updates

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,18 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+function MyCtrl1($resource, $http) {
+
+	$http.get('http://mdch-det-dev01:8080/api/json?pretty=true').success(function (data) {
+		console.log(data);
+	});
+
+	var View = $resource('http://mdch-det-dev01::port/api/json?tree=views[name,jobs[name]]', {port: '8080'});
+	var user = View.get();
+}
+
+MyCtrl1.$inject = ['$resource', '$http'];
+
 
 
 function MyCtrl2() {
