19274b625fecc5773ca239c8fd60eb83459d47fc
Gleb Godonoga <gleb.godonoga@gmail.com>
2013-01-18 19:45:58 +0200

    Small updates

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,32 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('jenkinsClient', ['jenkinsClient.filters', 'jenkinsClient.services', 'jenkinsClient.directives', 'ngResource']).config(
-	['$routeProvider', function ($routeProvider) {
+	['$routeProvider', '$provide', '$httpProvider', function ($routeProvider, $provide, $httpProvider) {
 		$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
 		$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
 		//$routeProvider.otherwise({redirectTo: '/view1'});
+
+		$provide.factory('myHttpInterceptor', function($q) {
+		  return function(promise) {
+		    return promise.then(function(response) {
+		    	if(response.headers()['content-type'] === "application/json;charset=UTF-8"){
+                // Validate response if not ok reject
+	                var data = response;
+
+	                if(!data)
+	                    return $q.reject(response);
+	            }
+		    	return response;
+		    }, function(response) {
+		      // do something on error
+		      if (canRecover(response)) {
+		        return response;
+		      }
+		      return $q.reject(response);
+		    });
+		  }
+		});
+
+		$httpProvider.responseInterceptors.push('myHttpInterceptor');
 	}]
 );
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,17 +3,19 @@
 /* Controllers */
 
 
-function MyCtrl1($resource, $http) {
+function MyCtrl1($scope, $resource, $http) {
 
 	$http.get('http://mdch-det-dev01:8080/api/json?pretty=true').success(function (data) {
 		console.log(data);
 	});
 
-	var View = $resource('http://mdch-det-dev01::port/api/json?tree=views[name,jobs[name]]', {port: '8080'});
-	var user = View.get();
+	var View = $resource('http://mdch-det-dev01::port/api/json?tree=views[name,jobs[name]]', {port: '8080'}, {
+		query: {method: 'GET', params: {}, isArray: false}
+	});
+	$scope.views = View.query();
 }
 
-MyCtrl1.$inject = ['$resource', '$http'];
+MyCtrl1.$inject = ['$scope', '$resource', '$http'];
 
 
 
