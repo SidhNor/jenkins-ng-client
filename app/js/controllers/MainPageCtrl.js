b43218d06294474bae09179a2fb10a971c4d5b7b
Gleb Godonoga <gleb.godonoga@gmail.com>
2013-01-30 16:04:29 +0200

    Added link cloacking

--- app/js/controllers/MainPageCtrl.js
+++ app/js/controllers/MainPageCtrl.js
@@ -3,10 +3,12 @@
 jenkinsClient.controller('MainPageCtrl', ['$scope', '$route', function MainPageCtrl($scope, $route) {
 	$scope.title = 'Dashboard';
 	
-	$scope.$on('$locationChangeSuccess', function(scope) {
+	$scope.$on('$locationChangeSuccess', function () {
 		if ($route.current) {
 			if ($route.current.params.hasOwnProperty('jobViewName')) {
 				$scope.title = 'View: ' + $route.current.params.jobViewName;
+			} else if ($route.current.params.hasOwnProperty('jobName')) {
+				$scope.title = 'Job: ' + $route.current.params.jobName;
 			}
 		}
 	});
